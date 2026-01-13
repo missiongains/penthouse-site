28605c8e25ed379139cd5565172c2ded21f3ce2f
missiongains <142062110+missiongains@users.noreply.github.com>
2026-01-13 15:29:50 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#020617',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
